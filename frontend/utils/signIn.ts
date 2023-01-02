34c4d64328c313c7895892117174cf9f30b84376
NinadRao0707 <ninadsrao@gmail.com>
2023-01-02 14:25:35 +0530

    Removing Frontend

--- frontend/utils/signIn.ts
+++
@@ -1,6 +0,0 @@
-import { SupabaseClient } from "@supabase/supabase-js";
-
-export default async function signIn(supabaseClient: SupabaseClient<any, "public", any>) {
-    const { error } = await supabaseClient.auth.signInWithOAuth({ provider: "google" });
-    if (error) console.log(error);
-}
