2ec64cfbbccff885bc733dd15b592c61503eb380
NinadRao0707 <ninadsrao@gmail.com>
2023-01-08 08:38:33 +0530

    🔥 Frontend changed to JS

---
+++ frontend/utils/teams.js
@@ -0,0 +1,28 @@
+const teams = [
+  {
+    profileUrl: "/../public/assets/ninadProfile.jpg",
+    name: "Ninad Rao",
+    role: "Team Leader",
+    linkedinUrl: "https://www.linkedin.com/in/ninadsrao/",
+    twitterUrl: "",
+    githubUrl: "https://github.com/NinadRao0707",
+  },
+  {
+    profileUrl: "/../public/assets/aamirProfile.jpg",
+    name: "Aamir Ansari",
+    role: "Team Member",
+    linkedinUrl: "https://www.linkedin.com/in/aamir-ansari-3b349b206",
+    twitterUrl: "",
+    githubUrl: "https://github.com/Aamir-Ansari-almost",
+  },
+  {
+    profileUrl: "/../public/assets/nimishaProfile.png",
+    name: "Nimisha Jain",
+    role: "Team Member",
+    linkedinUrl: "https://www.linkedin.com/in/nimisha-jain-1742b3214",
+    twitterUrl: "",
+    githubUrl: "https://github.com/Nimishajain77",
+  },
+];
+
+export default teams;
